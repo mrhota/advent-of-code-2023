0177b63800dc9d8aa63c3f979443ccf1d425db8e
AJ Gardner <mrhota@users.noreply.github.com>
2023-12-24 19:56:32 -0500

    Solve day 2, part 1

--- src/mrhota/day02.clj
+++ src/mrhota/day02.clj
@@ -1,36 +1,49 @@
 (ns mrhota.day02
   (:require [clojure.core.logic :as l]
-            [clojure.core.logic.arithmetic :as la]
-            [clojure.core.logic.pldb :as pldb]))
-
-;; can we use core.logic here?
-;; the puzzle involves constraints, and determining which of a set of items
-;; satisfy those constraints.
+            [clojure.core.logic.fd :as fd]
+            [clojure.test :refer [is]]
+            [mrhota.day02-input :as input]))
 
 ;; The Elf would first like to know which games would have been possible
 ;; if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
 
-;; "There exists a game consisting of `draws` draws."
-(pldb/db-rel game draws)
-;; the relation, draw, describes a game and counts of red, green, and blue pips
-;; you might say the predicate for this relation is:
-;; "There exists a draw for a game, `gm`, with counts `r`, `g`, and `b`."
-(pldb/db-rel draw gm r g b)
-
-(def draws
-  (pldb/db
-   [1 4 0 3]
-   [1 1 2 6]))
+;; Let's use core.logic.fd (for "finite domain") to describe the domains of
+;; possible values for each color in a draw. Let's call a "possible game"
+;; one in which the draws for each color are never greater than the values
+;; mentioned above. For example, in a "possible game", on any draw, the
+;; number of red cubes can be any number from 0 to 12, green cubes
+;; can be any number from 0 to 13, and blue cubes can be any number
+;; from 0 to 14.
+(def red-domain (fd/interval 12))
+(def green-domain (fd/interval 13))
+(def blue-domain (fd/interval 14))
 
 #_{:clj-kondo/ignore [:unresolved-symbol]}
-;; find all games
-(pldb/with-dbs [draws]
-  (l/run* [gm]
-          (l/fresh [red green blue]
-                   (la/<= red 12)
-                   (la/<= green 13)
-                   (la/<= blue 14)
-                   (draw gm red green blue))))
+(defn game-possible
+  "given a vector of game-id and a sequence of draws, return the game-id
+     if the game is possible, otherwise return nil."
+  [[game-id draws]]
+  ;; (run 1 ...) returns a list of 1 (or 0) result, but we don't want a list; so just take the first
+  (first
+   ;; we expect a maximum of 1 solution, so we use (run 1 ...) instead of (run* ...)
+   (l/run 1 [q]
+          ;; if a solution exists, q will unify (be the same as) game-id,
+          ;; every draw of reds in the game is in the red-domain,
+          ;; every draw of greens in the game is in the green-domain,
+          ;; and every draw of blues in the game is in the blue-domain
+          (l/== q game-id)
+          (l/everyg #(fd/in % red-domain) (keep :red draws))
+          (l/everyg #(fd/in % green-domain) (keep :green draws))
+          (l/everyg #(fd/in % blue-domain) (keep :blue draws)))))
+
+;; DAY 2, PART 1 SOLUTION
+;; ----------------------
+;; for each game in data, keep only the non-nil values
+;; returned by game-possible, then apply + to the resulting sequence
+(->> input/data
+     (keep game-possible)
+     (apply +))
+
 
 (comment
   ;; Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
@@ -40,7 +53,6 @@
   ;; Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
   ;; convert into a sequence of games. each game is a sequence of draws. each draw is a map
   ;; of colors to counts
-
 
   (def sample-game-data
     "a sequence of games. each game is a sequence of draws. each draw is a map
@@ -59,12 +71,27 @@
          {:green 3 :blue 15 :red 14}]]
      [5 [{:red 6 :blue 1 :green 3}
          {:blue 2 :red 1 :green 2}]]])
-  
-  (pldb/with-dbs [draws]
-    (l/run* [gm]
-            (l/fresh [game red green blue]
-                     (draw game red green blue)
-                     (la/<= red 12)
-                     (la/<= green 13)
-                     (la/<= blue 14))))
-  )
+
+  (game-possible [1 [{:red 4 :blue 3}
+                     {:red 1 :green 2 :blue 6}
+                     {:green 2}]])
+
+  (game-possible [3 [{:green 8 :blue 6 :red 20}
+                     {:blue 5 :red 4 :green 13}
+                     {:green 5 :red 1}]])
+  (keep :green [{:red 4 :blue 3}
+                {:red 1 :green 2 :blue 6}
+                {:green 2}])
+
+  (is (= (keep game-possible sample-game-data)
+         '(1 2 5)))
+  (is (= (apply + (keep game-possible sample-game-data))
+         8))
+
+  (map game-possible sample-game-data)
+
+  (defn game-determination
+    [game-data]
+    (map + (map game-possible game-data)))
+
+  :rcf)
